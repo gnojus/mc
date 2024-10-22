1ff8adc00593e20bd351d2b594cce1e7e8bd1bc1
Yury V. Zaytsev <yury@shurup.com>
2024-10-22 13:24:30 +0200

    Ticket #2452: handle -1 (baudrate error) by *not* disabling verbose mode implicitly
    If we can't reliably identify slow terminal, we should not implicitly disable
    verbose mode, instead let user disable it manually by starting with the slow
    terminal switch.
    
    Signed-off-by: Yury V. Zaytsev <yury@shurup.com>

--- src/filemanager/filemanager.c
+++ src/filemanager/filemanager.c
@@ -854,7 +854,7 @@ setup_mc (void)
 #endif /* HAVE_CHARSET */
 #endif /* HAVE_SLANG */
 
-    if ((tty_baudrate () < 9600) || mc_global.tty.slow_terminal)
+    if ((tty_baudrate () > 0 && tty_baudrate () < 9600) || mc_global.tty.slow_terminal)
         verbose = FALSE;
 }
 
